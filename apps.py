e487ef17669f8447530a81853af95e6ade0f65ae
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-08-08 15:15:05 -0500

    ok

---
+++ apps.py
@@ -0,0 +1,28 @@
+import constants
+
+team_copy = constants.TEAMS
+players_copy = constants.PLAYERS
+
+
+
+
+def clean_data():
+    for key, values in players_copy.items():
+        print(key)
+        print(values)
+
+
+
+    
+    
+    
+
+
+        
+    
+
+
+
+if __name__ == "__main__":
+    print('hello')
+    
