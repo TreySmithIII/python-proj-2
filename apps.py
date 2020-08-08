5095e3a05a23de3cc0f765c7ab2c99564e3042c0
TreySmithIII <56052484+TreySmithIII@users.noreply.github.com>
2020-08-08 15:34:10 -0500

    hello

--- apps.py
+++ apps.py
@@ -3,13 +3,12 @@ import constants
 team_copy = constants.TEAMS
 players_copy = constants.PLAYERS
 
-
-
+players_height = []
 
 def clean_data():
-    for key, values in players_copy.items():
-        print(key)
-        print(values)
+    for height in players_copy(0 , 3):
+        height.appendint((players_height))
+        
 
 
 
@@ -25,4 +24,3 @@ def clean_data():
 
 if __name__ == "__main__":
     print('hello')
-    
